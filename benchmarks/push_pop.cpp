5f338429010a51b175e34859a77c059c248a6d84
Marvin Williams <williams@kit.edu>
2024-05-03 19:07:58 +0200

    removed output

--- benchmarks/push_pop.cpp
+++ benchmarks/push_pop.cpp
@@ -332,7 +332,6 @@ class Context : public thread_coordination::Context {
         if (context.id() == 0) {
             auto end = std::chrono::high_resolution_clock::now();
             context.shared_data().push_times[step] = end - context.shared_data().start_time;
-            std::clog << "Step " << step << " done\n";
         }
 #ifdef WITH_PAPI
         if (!context.settings().papi_events.empty()) {
@@ -383,7 +382,6 @@ class Context : public thread_coordination::Context {
         if (context.id() == 0) {
             auto end = std::chrono::high_resolution_clock::now();
             context.shared_data().pop_times[step] = end - context.shared_data().start_time;
-            std::clog << "Step " << step << " done\n";
         }
 #ifdef WITH_PAPI
         if (!context.settings().papi_events.empty()) {
