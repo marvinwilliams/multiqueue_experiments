de5782fb4ccc97eb5153af9df62ad1402f936439
Marvin Williams <williams@kit.edu>
2024-05-07 17:33:14 +0200

    updated description

--- wrapper/mq_pq.hpp
+++ wrapper/mq_pq.hpp
@@ -5,7 +5,7 @@
 #include "wrapper/multiqueue.hpp"
 #undef MQ_MODE_RANDOM
 
-#include <atomic>
+#include <mutex>
 
 #ifdef __SSE2__
 #include <emmintrin.h>
@@ -27,62 +27,46 @@ class MultiQueuePQ {
    private:
     using pq_type = typename wrapper::multiqueue::MultiQueue<Min, Key, T>::pq_type;
 
-    std::atomic_bool lock_{};
+    std::mutex lock_{};
     pq_type pq_{};
 
    public:
     using handle_type = util::SelfHandle<MultiQueuePQ>;
     using settings_type = util::EmptySettings;
 
-   private:
-    void lock() {
-        /* while (true) { */
-        /*     while (lock_.load(std::memory_order_relaxed)) { */
-        /*         PAUSE; */
-        /*     } */
-        /*     if (!lock_.exchange(true, std::memory_order_acquire)) { */
-        /*         return; */
-        /*     } */
-        /* } */
-        while (true) {
-            if (!lock_.exchange(true, std::memory_order_acquire)) {
-                return;
-            }
-            do {
-                PAUSE;
-            } while (lock_.load(std::memory_order_relaxed));
-        }
-    }
-
-    void unlock() {
-        lock_.store(false, std::memory_order_release);
-    }
-
-   public:
     MultiQueuePQ(int /*unused*/, std::size_t initial_capacity, settings_type const& /*unused*/) {
         pq_.reserve(initial_capacity);
     }
 
     void push(value_type const& value) {
-        lock();
+        std::scoped_lock<std::mutex> guard(lock_);
         pq_.push(value);
-        unlock();
     }
 
     std::optional<value_type> try_pop() {
-        lock();
+        std::scoped_lock<std::mutex> guard(lock_);
         if (pq_.empty()) {
-            unlock();
             return std::nullopt;
         }
         auto retval = pq_.top();
         pq_.pop();
-        unlock();
         return retval;
     }
 
     static void write_human_readable(std::ostream& out) {
-        out << "MultiQueue PQ\n";
+        out << "MultiQueuePQ" << '\n';
+#ifdef MQ_USE_BTREE
+        out << "  PQ: tlx::btree" << '\n';
+#else
+#ifdef MQ_USE_STD_PQ
+        out << "  PQ: std::priority_queue" << '\n';
+#else
+        out << "  PQ: d-ary heap" << '\n';
+        out << "  Heap arity: " << wrapper::multiqueue::heap_arity << '\n';
+#endif
+        out << "  Insertion buffer size: " << wrapper::multiqueue::insertion_buffer_size << '\n';
+        out << "  Deletion buffer size: " << wrapper::multiqueue::deletion_buffer_size << '\n';
+#endif
     }
 
     handle_type get_handle() {
